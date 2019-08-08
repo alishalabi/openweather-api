2e3f26fbca7af9b576ebd9cc63f2d3dd4f9f6364
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-08-07 19:37:23 -0700

    Added homepage and search bar

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const express = require('express')
 const app = express()
 
 // Middleware
+const exphbs  = require('express-handlebars')
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello Weather')
+  res.render('home')
 })
 
 // Start Server
