9477f2c2a085ccc75058935a4443e984b0467420
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-08-13 14:06:11 -0700

    Body parser installed and working

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Connect environmental variables
+require('dotenv').config()
+
 // Libraries
 const express = require('express')
 
@@ -6,17 +9,27 @@ const app = express()
 
 // Middleware
 const exphbs  = require('express-handlebars')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+
 // Routes
+// Home
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+// Post
+app.post('/', function (req, res) {
+  res.render('home')
+  console.log(req.body.city)
 })
 
 // Start Server
 
 app.listen(8000, () => {
-  console.log("Openweather App Running On Port 8000")
+  console.log('Openweather App Running On Port 8000')
 })
