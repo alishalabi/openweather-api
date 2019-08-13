8a6cd39a4e5d3075c5b1834663bf4cd94187b556
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-08-13 14:59:54 -0700

    Got request npm to work

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const express = require('express')
 // Instantiate
 const app = express()
 
+// Environmental Variables
+const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY
+
 // Middleware
 const exphbs  = require('express-handlebars')
 const bodyParser = require('body-parser')
+const request = require('request')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
@@ -23,9 +27,27 @@ app.get('/', (req, res) => {
 })
 
 // Post
-app.post('/', function (req, res) {
-  res.render('home')
-  console.log(req.body.city)
+app.post('/', (req, res) => {
+  const city = req.body.city
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPENWEATHER_API_KEY}`
+  console.log(url)
+  // "Request" syntax inspired by https://gist.github.com/bmorelli25/c67347560fe7b2213fea0d32d66fa53f#file-server-js
+  request(url, (err, response, body) => {
+    if(err){
+      res.render('city', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('city', {weather: null, error: 'Error, please try again'});
+      } else {
+        // let weatherMessage = `It's ${weather.main.temp} degrees in ${weather.name}!`
+        res.render('city', {weather: weather, error: null});
+      }
+    }
+  })
+
+  // res.render('city')
+  // console.log(req.body.city)
 })
 
 // Start Server
